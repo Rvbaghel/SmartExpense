3b2f9ec42527e007227a22ba397f8d1d559929b8
Vishal Baghel <baghelvishal264@gmail.com>
2025-09-26 04:57:11 +0530

    Final

--- backend/app.py
+++ backend/app.py
@@ -2,19 +2,19 @@ from flask import Flask
 from flask_cors import CORS
 from routers.auth_routes import auth_bp
 from routers.salary_routes import salary_bp
-from routers.category_routes import category_bp   # 👈 import category routes
+from routers.category_routes import category_bp
 from routers.expense_router import expense_bp
 from routers.dashboard_routes import dashboard_bp
 
-
-
 app = Flask(__name__)
-CORS(app, origins=["http://localhost:5173"],supports_credentials=True)  # change port if needed
 
-# ✅ Register Blueprints (routers)
+# ✅ Update CORS to allow your deployed frontend
+CORS(app, origins=["http://localhost:5173", "https://smart-expense-beta.vercel.app"], supports_credentials=True)
+
+# Register Blueprints
 app.register_blueprint(auth_bp, url_prefix="/auth")
 app.register_blueprint(salary_bp, url_prefix="/salary")
-app.register_blueprint(category_bp, url_prefix="/category")   # 👈 register here
+app.register_blueprint(category_bp, url_prefix="/category")
 app.register_blueprint(expense_bp)
 app.register_blueprint(dashboard_bp)
 
